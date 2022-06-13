f4055147cd998b93823e2ca35c60faca2b09d947
seulki9802 <calop@naver.com>
2022-06-13 19:08:26 +0900

    회원가입시 조건 달기. id, pw 공백이나 특수문자 입력 불가. 한글 불가. 길이 제한. 숫자만 쓰는 거 제한. pw는 영어만 쓰는 것도 제한

--- react-board/src/page/sign/SignUp.js
+++ react-board/src/page/sign/SignUp.js
@@ -7,10 +7,23 @@ function SignUp() {
 
   function signup() {
 
+    //input id, pw
     var id = $('.SignUp input[name=id]').val(),
         pw = $('.SignUp input[name=pw]').val();
 
-    
+    //next situation -> can't sign up
+    const engNum = /^[|a-z|0-9|]+$/,
+          eng = /^[|a-z|]+$/,
+          num = /^[|0-9|]+$/;
+
+    if (!id) return alert('아이디를 입력하세요.')
+    if (!pw) return alert('비밀번호를 입력하세요.')
+    if (!(engNum.test(id) && !num.test(id))) return alert('아이디는 영문 또는 영문+숫자만 입력 가능합니다.')
+    if (!(engNum.test(pw) && !num.test(pw) && !eng.test(pw))) return alert('비밀번호는 영문+숫자만 입력 가능합니다.')
+    if (id.length < 3 ||  id.length > 10) return alert('아이디는 4자 이상 10자 이내로 만들어주세요.')
+    if (pw.length < 6 || pw.length > 20) return alert('비밀번호는 6자 이상 20자 이내로 작성해주세요.')
+
+    //requst sign up to server!
     $.ajax({
       method: 'post',
       url: '/sign/up',
@@ -27,8 +40,8 @@ function SignUp() {
   return (
     <div className="SignUp">
       <h2>Sign Up</h2>
-      <input name='id'/><br/>
-      <input name='pw'/><br/>
+      <input name='id' placeholder='4~10자 (영문 / 영문 + 숫자)'/><br/>
+      <input name='pw' type='password' placeholder='6~20자 (영문 + 숫자)'/><br/>
       <button onClick={ signup }>sign up</button>
     </div>
       
